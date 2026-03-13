21dde71c56bb9772843a29bb5519012a0a3030cb
Anver <anvershajahan123@gmail.com>
2026-03-13 11:58:22 +0530

    Enable traceback and gunicorn logs for Render debugging

--- wedify_project/wsgi.py
+++ wedify_project/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+import traceback
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wedify_project.settings')
 
-application = get_wsgi_application()
+try:
+    application = get_wsgi_application()
+except Exception as e:
+    print(f"WSGI LOAD ERROR: {e}", file=sys.stderr)
+    traceback.print_exc(file=sys.stderr)
+    raise
